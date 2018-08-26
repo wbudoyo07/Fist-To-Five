2bb8d004965330c282517ddf933e97d67c61b8c6
William Budoyo <williambu07@gmail.com>
2018-08-26 09:36:17 -0400

    able to render first index in array to handlebars

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,17 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-  // store infomation data
+  //GET  store infomation data
   app.get("/api/register", function(req, res) {
     db.storeInfo.findAll({}).then(function(database) {
       res.json(database);
     });
   });
-  // Create a new example
+  // Create a new owner's information
   app.post("/api/register", function(req, res) {
     console.log(req.body);
     db.storeInfo.create(req.body).then(function(database) {
       res.json(database);
-    });
-  });
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
     });
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,8 @@ var path = require ("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    res.render("home", {
+      titlePage: "Home"
     });
   });
 
@@ -22,7 +19,7 @@ module.exports = function(app) {
   app.get("/results", function(req, res) {
     db.storeInfo.findAll({}).then(function(dbStoreInfo) {
       res.render("results", {
-        titlePage: "results page",
+        titlePage: "Results",
         dataInfo: dbStoreInfo
       });
     });
@@ -31,15 +28,6 @@ module.exports = function(app) {
   // app.get("/results", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/html/results.html"));
   // });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
