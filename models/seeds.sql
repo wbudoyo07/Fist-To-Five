fb4fda071dff41bc9c364b212cc5e03bc866e6cc
Katie Lytch <klytch@gmail.com>
2018-08-31 14:06:52 -0400

    changes to front-end

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,15 +1,16 @@
+
 USE masseuseDB;
 
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Bianca', 'Miller', '3479  Woodridge Lane', 'Dimassage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 100);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Octavia', 'Marcy', '4287  Worley Avenue', 'Massagefy', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 101);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Jacqueline', 'Dixon', '3078  Drummond Street', 'La Massage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 102);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Kevin', 'Hickey', '517  Whitman Court', 'Massage Fix', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 103);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Kelly', 'Bynum', '479  Coffman Alley', 'Retro Massage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 104);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Carmen', 'Barnes', '954  Meadow View Drive', 'Massageyo', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 105);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Mary', 'Hammond', '2790  Golf Course Drive', 'Leemassage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 106);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Gary', 'Crowder', '1497  Newton Street', 'Mega Massage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 107);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Perry', 'Morales', '131  Melody Lane', 'Zoomassage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 108);
-INSERT INTO storeInfos (fname, lname, address, storeName, description, price) VALUES ('Kirstin', 'Suter', '4287  Worley Avenue', 'Massageble', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 109);
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Bianca', 'Bianca', 'Miller', '3479  Woodridge Lane', 'Dimassage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 100, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Octavia', 'Octavia', 'Marcy', '4287  Worley Avenue', 'Massagefy', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 101, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Jacqueline', 'Jacqueline', 'Dixon', '3078  Drummond Street', 'La Massage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 102, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Kevin', 'Kevin', 'Hickey', '517  Whitman Court', 'Massage Fix', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 103, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Kelly', 'Kelly', 'Bynum', '479  Coffman Alley', 'Retro Massage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 104, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Carmen', 'Carmen', 'Barnes', '954  Meadow View Drive', 'Massageyo', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 105, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Mary', 'Mary', 'Hammond', '2790  Golf Course Drive', 'Leemassage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 106, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Gary', 'Gary', 'Crowder', '1497  Newton Street', 'Mega Massage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 107, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Perry', 'Perry', 'Morales', '131  Melody Lane', 'Zoomassage', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 108, "http://placehold.it/900x400");
+INSERT INTO storeInfos (routeName, fname, lname, address, storeName, description, price, img) VALUES ('Kirstin', 'Kirstin', 'Suter', '4287  Worley Avenue', 'Massageble', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 109, "http://placehold.it/900x400");
 
 
 INSERT INTO customerReviews (fname, lname, textReview, scoreValues) VALUES ('James', 'Hatfield', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 5);
