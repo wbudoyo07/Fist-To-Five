427c62609fd96326d32c218b417c837380a3afc3
Jdelorim <joshuadelorimier@gmail.com>
2018-09-06 21:10:46 -0400

    need to relink to sequalize database...pic is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,27 @@
 var db = require("../models");
-// var QRcode = require("qrcode");
-// var fs = require("fs");
+var QRcode = require("qrcode");
+var fs = require("fs");
 var passport = require("../config/passport");
+var multer = require("multer");
+var path = require("path");
 
 
 module.exports = function (app) {
 
-  // var storeID;
+  var storeID;
   var storeRoute;
-  // var base64Data;
+  var base64Data;
+
+  var storage = multer.diskStorage({
+    destination: "./public/images/profileImgs",
+    filename: function(req, file, next) {
+      next(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+  var upload = multer({
+    storage: storage
+  }).single("storeImg");
   // GET the information data from login user
   app.get("/api/login", function (req, res) {
     res.json(req.user);
@@ -33,13 +46,18 @@ module.exports = function (app) {
   });
 
   // Create a new owner's information
-  app.post("/api/register", function (req, res) {
-    // console.log("store the new id:",req.body);
+  app.post("/api/register",upload, function (req, res) {
+    res.send("connected");
+    //these work
+    console.log("fname:",req.body.fname);
+    console.log("price:",req.body.priceNumber);
+    //need to pass to sequalize
+    storeRoute = req.body.routeName;
+    
     db.storeInfo.create(req.body).then(function (database) {
       // res.json(database);
-      storeRoute = database.routeName;
-      storeID = database.id;
-
+      
+      console.log("routeName:",storeRoute);
       res.redirect(307,"/api/login");
     //  console.log("store the new id:",database.id);
     }).then(function() {
@@ -64,33 +82,21 @@ module.exports = function (app) {
     QRcode.toDataURL(siteToGenerate, function (err, pic) {
       var imgData = pic;
       base64Data = imgData.replace(/^data:image\/png;base64,/, "");
-
-
       db.storeInfo.update({
-
         QRcode: imgData
       }, {
         where: {
           id: storeID
         }
-
-      }).then(function(database) {
-
+      }).then(function() {
         writeQRtoFile();
       });
-
     });
   }
 
   function writeQRtoFile() {
-
-    var convertIDtoString = storeID.toString();
-    console.log("ID?:", "./images/QRcodeID:" + convertIDtoString + ".png");
-
-    var saveLocation = "./images/QRcodeID" + convertIDtoString + ".png";
-
-
-
+    var saveLocation = "./public/images/qrCodeImages/QRcodeID" + storeRoute + ".png";
+    
     fs.writeFile(saveLocation, base64Data, "base64", function (err) {
       if (err) {
         console.log("Error writing file:", err);
