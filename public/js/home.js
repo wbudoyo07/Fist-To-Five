86f72e64c12dbb0ae5d1c365f8e6076ad104e626
Katie Lytch <klytch@gmail.com>
2018-09-07 13:51:52 -0400

    styling and store cards

--- public/js/home.js
+++ public/js/home.js
@@ -23,22 +23,3 @@ $(document).ready(function() {
   }
 });
 
-
-function runStoreQuery() {
-  var currentURL = window.location.origin;
-
-  // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
-  $.ajax({ url: currentURL + "/api/register", method: "GET" })
-    .then(function(storeData) {
-
-      for (var i = 0; i < 3; i++) {
-
-        var storeSection = $("<div>");
-        storeSection.addClass("well");
-        storeSection.attr("id", "storeWell-" + i + 1);
-        $("#storeSection").append(storeSection);
-        $("#storeWell-" + i + 1).append("<div class='col-sm-4 pull-left'><div class='card'><img src='" + storeData[i].img + "'style='width:100%'><div class='card-body'><h3>" + storeData[i].fname + " " + storeData[i].lname +"</h3><h3 class='w3-opacity'> $" + storeData[i].price + "</h3><h3>" + storeData[i].storeName + "</h3><h4>" + storeData[i].address + "</h4><button id='search-button' class='btn btn-secondary btn-md btn-block'><a href=" + "results/" + storeData[i].routeName + ">Read Reviews</a></button></div></div></div>");            
-      }
-    });
-}
-runStoreQuery();
